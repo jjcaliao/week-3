0bdf777be6d5c1c0ef56b2701a3c542a9a5ae80e
jjcaliao <jcaliao098@gmail.com>
2022-07-23 17:59:23 +0100

    added html

--- javascript.js
+++ javascript.js
@@ -4,6 +4,11 @@ var choices = Array("rock", "paper", "scissors");
 var cscore = 0;
 var pscore = 0;
 
+var computerScore = document.getElementById("cscore");
+var playerScore = document.getElementById("pscore");
+var result = document.getElementById("result");
+var btns = document.querySelectorAll("button");
+
 function computerPlay(){ //randomly generates choice
     let choice = choices[Math.floor(Math.random()*choices.length)];
     return choice;
@@ -48,27 +53,46 @@ function round(playerChoice, computerChoice){ //generates outcomes for each roun
     }
 }
 
+btns.forEach(possibleChoice => possibleChoice.addEventListener('click', function(e){
+    let pchoice = e.target.id;
+    let cchoice = computerPlay();
+    result.innerHTML = round(pchoice, cchoice);
+    computerScore.innerHTML = cscore;
+    playerScore.innerHTML = pscore;
+    if(pscore == 5){
+        for (const btn of btns) {
+            btn.disabled = true
+        }
+        result.innerHTML = "You win!";
+    }
+    else if(cscore == 5){
+        for (const btn of btns) {
+            btn.disabled = true
+        }
+        result.innerHTML = "Computer wins!";
+    }
+}));
+
 //var c = "rock";
 //console.log(round(c, computerPlay()));
 
-function game(){ //generates 5 rounds
-    for(let i=0; i<5; i++){
-        let playerc = window.prompt("rock, paper or scissors?").toLowerCase();
-        let computerc = computerPlay();
-        console.log("Computer picked " + computerc + " and you picked " + playerc);
-        round(playerc, computerc);
-        console.log(round(playerc, computerc));
-        console.log("Computer: " + cscore/2 + " You: " + pscore/2);
-    }
-    if (cscore > pscore){
-        console.log("You lose!");
-    }
-    else if(pscore > cscore){
-        console.log("You win!");
-    }
-    else if(pscore == cscore){
-        console.log("It's a tie!");
-    }
-}
+// function game(){ //generates 5 rounds
+//     for(let i=0; i<5; i++){
+//         let playerc = window.prompt("rock, paper or scissors?").toLowerCase();
+//         let computerc = computerPlay();
+//         round(playerc, computerc);
+//         console.log(round(playerc, computerc));
+//         console.log("Computer: " + cscore/2 + " You: " + pscore/2);
+//     }
+//     if (cscore > pscore){
+//         console.log("You lose!");
+//     }
+//     else if(pscore > cscore){
+//         console.log("You win!");
+//     }
+//     else if(pscore == cscore){
+//         console.log("It's a tie!");
+//     }
+// }
 
-game();
+// game();
